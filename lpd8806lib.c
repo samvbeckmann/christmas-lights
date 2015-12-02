86a9a48310dbc55828f439cea5e624ab36ed1563
Sam Beckmann <samvbeckmann@gmail.com>
2015-12-01 22:11:22 -0600

    Added functionality to get pixel color, pass compact colors to the LED library.

--- lpd8806lib.c
+++ lpd8806lib.c
@@ -8,7 +8,6 @@
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 
-
 /*
  * This library is not particulary robust at the moment, as it only supports LED strips 32 LEDs in length.
  * For some reason there no data is saved if I attempt to use a dynamically allocated array.
@@ -21,9 +20,9 @@
 
 uint8_t buffer[97];
 uint16_t bufferLen = 97;
+uint16_t numLEDs = 32;
 
-void initLEDStrip()
-{
+void initLEDStrip() {
 	// Prep the GPIO for transmitting.
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 	GPIOPinTypeGPIOOutput( GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
@@ -31,15 +30,13 @@ void initLEDStrip()
 	// Send an empty byte to clear any buffer, prepare the strip to recieve information.
 	GPIOPinWrite(GPIO_PORTB_BASE, DATA_PIN, 0);
 	int i;
-	for (i = 0; i < 8; i++)
-	{
+	for (i = 0; i < 8; i++) {
 		GPIOPinWrite( GPIO_PORTB_BASE, CLOCK_PIN, CLOCK_PIN);  // clock high
 		GPIOPinWrite( GPIO_PORTB_BASE, CLOCK_PIN, 0);  		   // clock low
 	}
 }
 
-void showStrip()
-{
+void showStrip() {
 	// Step through the passed buffer, pushing the whole thing to the strip
 	uint16_t i;
 	for (i = 0; i < bufferLen; i++) {
@@ -47,8 +44,7 @@ void showStrip()
 
 		// MSB first
 		uint8_t b;
-		for (b = 7;; b--)
-		{
+		for (b = 7;; b--) {
 			GPIOPinWrite( GPIO_PORTB_BASE, DATA_PIN, (aByte >> b));
 
 			GPIOPinWrite( GPIO_PORTB_BASE, CLOCK_PIN, CLOCK_PIN);  // clock high
@@ -60,11 +56,33 @@ void showStrip()
 	}
 }
 
+uint32_t getPixelColor(uint16_t idx) {
+	if (idx < numLEDs) {
+		uint16_t offset = idx * 3;
+		return ((uint32_t) (buffer[offset] & 0x7f) << 16)
+				| ((uint32_t) (buffer[offset + 1] & 0x7f) << 8)
+				| (uint32_t) (buffer[offset + 2] & 0x7f);
+	}
+
+	return 0; // Pixel # is out of bounds
+}
+
+void setRGBCompact(uint16_t idx, uint32_t color) {
+	if (idx < numLEDs) { // Arrays are 0-indexed, thus NOT '<='
+		uint16_t offset = idx * 3;
+		buffer[offset] = (color >> 16) | 0x80;
+		buffer[offset + 1] = (color >> 8) | 0x80;
+		buffer[offset + 2] = color | 0x80;
+	}
+}
+
 // Valid color ranges: 0x0...0x7f
-void setRGB(uint16_t idx, uint8_t r, uint8_t g, uint8_t b)
-{
-	uint16_t offset = idx * 3;
-	buffer[offset] = g | 0x80;
-	buffer[offset + 1] = r | 0x80;
-	buffer[offset + 2] = b | 0x80;
+void setRGB(uint16_t idx, uint8_t r, uint8_t g, uint8_t b) {
+	if (idx < numLEDs) {
+		uint16_t offset = idx * 3;
+		buffer[offset] = g | 0x80;
+		buffer[offset + 1] = r | 0x80;
+		buffer[offset + 2] = b | 0x80;
+	}
+
 }
